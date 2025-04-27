b3f8a73028557bdb028b6d876bd5b207aa61a83d
sonic-dre <sonicdrake7777@gmail.com>
2025-04-27 15:56:31 +0300

    ignore eslint and typscript errors

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent"
+
+const page = () => {
+  return (
+    <>
+    <h3>Interview Generation</h3>
+
+    <Agent userName ="you" userId= "user1" type = "generate" />
+    </>
+  )
+}
+
+export default page
