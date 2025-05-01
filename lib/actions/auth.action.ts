5d202d55a2f1df029b307a3d0d0576c14771a656
sonic-dre <sonicdrake7777@gmail.com>
2025-05-01 09:59:25 +0300

    Show generated interviews on the home peage

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { auth, db } from "@/firebase/admin";
+import { CollectionReference, DocumentData, Query } from "firebase-admin/firestore";
 import { cookies } from "next/headers";
 
 const ONE_WEEK = 60*60*24*7;
@@ -114,4 +115,35 @@ export async function isAuthenticated(){
     const user = await getCurrentUser();
 
     return !!user; //true 
+}
+
+export async function getInterviewByUserId(userId: string) : Promise<Interview[] | null> {
+    const interviews = await db
+    .collection('interviews')
+    .where('userId', '==', userId )
+    .orderBy('createdAt', 'desc')
+    .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
+}
+
+
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams) : Promise<Interview[] | null> {
+    const {userId, limit = 20} = params;
+    const interviews = await db
+    .collection('interviews')
+    .orderBy('createdAt', 'desc')
+    .where('finalized', '==', true )
+    .where('userId', '!=', userId )
+    .limit(limit)
+    .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
 }
